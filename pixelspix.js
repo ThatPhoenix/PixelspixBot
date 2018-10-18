92987e0680d02c378a601735da83de7a97fbb470
Phoenix <theemaraldblock@gmail.com>
2018-10-18 17:09:59 +0100

    Update pixelspix.js

--- pixelspix.js
+++ pixelspix.js
@@ -1,3 +1,10 @@
 const Discord = require('discord.js')
 const Pixel = new Discord.Client
-const Data = require("./Data.json")
+const Data = require("./data.json")
+
+Pixel.on('ready', () => {
+Pixel.user.setActivity("FIRST STARTUP YAY!")
+
+
+Pixel.login(process.env.PixelTK)
+// Don't think you can get away this time with hacking Pixelspix ;)
