1f116074a1cc2a644ced458e4d5f38e431377a94
Phoenix <theemaraldblock@gmail.com>
2018-11-10 12:56:53 +0000

    Update pixelspix.js

--- pixelspix.js
+++ pixelspix.js
@@ -1,11 +1,29 @@
-const Discord = require('discord.js')
-const Pixel = new Discord.Client
-const Data = require("./data.json")
+const Discord = require('discord.js');
+const Pixel = new Discord.Client();
+const fs = require('fs')
+const settings = require('./settings.json')
+require('./events/Handler.js')(Pixel);
 
-Pixel.on('ready', () => {
-Pixel.user.setActivity("FIRST STARTUP YAY!")
+Pixel.on("message", message => {
+  if (message.author.bot) return;
+  if(message.content.indexOf(settings.prefix) !== 0) return;
+//Command Loader
+  const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(Pixel, message, args);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
+Pixel.on('warn', e => {
+  console.log(e);
+});
 
+Pixel.on('error', e => {
+  console.log(e);
+});
 Pixel.login(process.env.PixelTK)
 // Don't think you can get away this time with hacking Pixelspix ;)
