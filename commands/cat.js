85a7c15364bbdb468180a9fbb303ee3f376b77dc
Phoenix <theemaraldblock@gmail.com>
2018-12-04 19:17:04 +0000

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,12 +1,23 @@
 //Requires Pixelspix Cat Database
+const talkedRecently = new Set();
+//Pixelspix Database Cooldown
 
+
+if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    } else {
 const CatData = require('../CatDat.json');
 exports.run = function(Pixel, message, args) {
   message.channel.sendMessage("A wild cute cat has appeared!")
       message.channel.send(CatData[Math.floor(Math.random() * 4) +1]);
   
 };
-
+        talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+    }
 exports.help = {
   name: 'cat'
 };
