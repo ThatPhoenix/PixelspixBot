57543a6df86985e2cf6845c0f6729b0bcae9046c
Phoenix <theemaraldblock@gmail.com>
2018-12-07 19:43:20 +0000

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,15 +1,11 @@
-//Requires Pixelspix Cat Database
-const talkedRecently = new Set();
 
-
+const CatDat = require('../CatDat.json');
 exports.run = function(Pixel, message, args) {
+  message.channel.sendMessage("A Wild Cute Cat has apeared")
+      message.channel.send(CatDat[Math.floor(Math.random() * 4) +1]);
+  
+};
 
-const CatData = require('../CatDat.json');
-exports.run = function(Pixel, message, args) {
-  message.channel.sendMessage("A wild cute cat has appeared!")
-      message.channel.send(CatData[Math.floor(Math.random() * 4) +1]);
-}
-}
 exports.help = {
   name: 'cat'
 };
