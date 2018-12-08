2da61e5e15fb6cd4ed7dfc9c9c23f28dc559b189
Phoenix <theemaraldblock@gmail.com>
2018-12-08 15:41:22 +0000

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,7 +2,7 @@
 const CatDat = require('../CatDat.json');
 exports.run = function(Pixel, message, args) {
   message.channel.sendMessage("A Wild Cute Cat has apeared")
-      message.channel.send(CatDat[Math.floor(Math.random() * 4) +1]);
+      message.channel.send(CatDat[Math.floor(Math.random() * 5) +1]);
   
 };
 
