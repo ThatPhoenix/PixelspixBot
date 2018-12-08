e16fea050cee05692f697abb43c6a17b0763c3d0
Phoenix <theemaraldblock@gmail.com>
2018-12-08 12:26:16 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,14 @@
 const settings = require("../settings.json")
+
 exports.run = function(Pixel, message, args) {
- message.channel.sendMessage('**Playing Ping Pong with Phoenix**')
+ 
+ message.channel.send('**Playing Ping Pong with Phoenix**')
     .then(message => {
       message.edit(`Pong! :ping_pong: **my ping is** ${message.createdTimestamp - message.createdTimestamp}ms  **DiscordAPI's ping is** ${Pixel.ping}`);
-    });
+  
 };  
-};
+          };
+
 
 exports.help = {
   name: 'ping'
