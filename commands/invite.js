5756fa4be2e84b050f3b371ed4c01daf678659c7
Phoenix <theemaraldblock@gmail.com>
2018-12-09 11:17:56 +0000

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,10 @@
+const CatDat = require('../CatDat.json');
+exports.run = function(Pixel, message, args) {
+  message.channel.send("Hi I'm currently on Alpha Please wait until i get fully built")
+  
+  
+};
+
+exports.help = {
+  name: 'invite'
+};
