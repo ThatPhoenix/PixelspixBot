4fe631a9970a666565d76610bf8e79fcbd017a24
Phoenix <theemaraldblock@gmail.com>
2018-12-15 09:23:00 +0000

    Create credits.js

---
+++ commands/credits.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const green = '#008000';
+const red = '#FF0000';
+const yellow = '#FFD700';
+
+exports.run = function(Pixel, message, args) {
+message.channel.send(`Credits:`)
+message.channel.send("Owners: ThatPhoenix, HasanBrandsXD| Administrators: NUT NUT, Elcobrox")
+};
+
+
+
+exports.help = {
+name: 'test'
+};
