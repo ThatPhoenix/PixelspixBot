b7afe0c2a30024ae3a4f8181b6f582cdec884e7d
Phoenix <theemaraldblock@gmail.com>
2019-01-11 20:35:06 +0000

    Create events.js

---
+++ utils/events.js
@@ -0,0 +1,6 @@
+const reqEvent = (event) => require(`../events/${event}`);
+module.exports = Pixel => {
+  Pixel.on('ready', () => reqEvent('ready')(Pixel));
+  Pixel.on('reconnecting', () => reqEvent('reconnecting')(Pixel));
+  Pixel.on('disconnect', () => reqEvent('disconnect')(Pixel));
+};
