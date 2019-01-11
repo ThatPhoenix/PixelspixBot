4e4c5d5abd4b34428cdb82ea529ced98d128a9ee
Phoenix <theemaraldblock@gmail.com>
2019-01-11 21:24:42 +0000

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 
-exports.run = function(Anthony, message, args) {
+exports.run = function(Pixel, message, args) {
   var embed = new Discord.RichEmbed();
-  embed.setTitle('Anthony Cmds')
+  embed.setTitle('Pixelspix Commands')
   embed.setDescription(':rofl: | **Fun Commands **: `!cat` `!coinflip` \n:tools: | **Moderation Commands:** `!ban` `!kick` `report`\n:information_source: | **Info Commands** `!invite` `!credits` `!ping`');
   embed.setColor('#FF0000');
   embed.addField('No Patreons yet.')
-  embed.setFooter('We hope you have fun with Anthony')
+  embed.setFooter('We hope you have fun with Pixelspix')
 
   message.channel.sendEmbed(embed);
 };
