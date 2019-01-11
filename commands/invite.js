f090013dfa15fa2c7943625a70f5303a9172a20d
Phoenix <theemaraldblock@gmail.com>
2019-01-11 21:32:59 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,10 +1,8 @@
-const CatDat = require('../CatDat.json');
-exports.run = function(Pixel, message, args) {
-  message.channel.send("Hi I'm currently on Alpha Please wait until i get fully built")
-  
-  
-};
+const Discord = require('discord.js');
 
-exports.help = {
-  name: 'invite'
+exports.run = async(Pixel, message, args, tools) => {
+
+      message.channel.send("Aww Your Inviting Me! Thank You!"); 
+    message.channel.send("**Invite URL: \nhttps://discordapp.com/api/oauth2/authorize?client_id=501802312604450820&permissions=8&scope=bot*")
+    message.channel.send("**Support Hub Invite: \n*https://discord.gg/utSTm8S*")
 };
