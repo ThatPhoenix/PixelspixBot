cd0965cc34ef21fc9ed0b8b0fe47e9a797b94bb2
Phoenix <theemaraldblock@gmail.com>
2019-01-11 20:28:39 +0000

    Update pixelspix.js

--- pixelspix.js
+++ pixelspix.js
@@ -4,20 +4,21 @@ const fs = require('fs')
 const settings = require('./settings.json')
 require('./events/Handler.js')(Pixel);
 
-Pixel.on("message", message => {
-  if (message.author.bot) return;
-  if(message.content.indexOf(settings.prefix) !== 0) return;
-//Command Loader
-  const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  try {
-    let commandFile = require(`./commands/${command}.js`);
-    commandFile.run(Pixel, message, args);
-  } catch (err) {
-    console.error(err);
-  }
-});
 
+Pixel.on("message", message => {
+    if (message.author.bot) return;
+    if(message.content.indexOf(settings.prefix) !== 0) return;
+  //Command Loader
+    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    try {
+      let commandFile = require(`./commands/${command}.js`);
+      commandFile.run(Pixel, message, args);
+    } catch (err) {
+           message.reply(`Uh Oh Something went wrong ${err}`)
+        
+    }
+})
 Pixel.on('warn', e => {
   console.log(e);
 });
