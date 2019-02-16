3237bd524a0f8ee057ecad54f133d966fa86f7cd
Phoenix <theemaraldblock@gmail.com>
2019-02-16 11:21:20 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,5 +4,5 @@ exports.run = async(Pixel, message, args, tools) => {
 
       message.channel.send("Aww Your Inviting Me! Thank You!"); 
     message.channel.send("**Invite URL: \nhttps://discordapp.com/api/oauth2/authorize?client_id=501802312604450820&permissions=8&scope=bot**")
-    message.channel.send("**Support Hub Invite: \nhttps://discord.gg/https://discord.gg/a4JyMYK**")
+    message.channel.send("**Support Hub Invite: \nhttps://discord.gg/a4JyMYK**")
 };
