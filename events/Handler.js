8e6124df66f5367e36fcb7a5ee3e7980c11f42ca
Phoenix <theemaraldblock@gmail.com>
2019-02-24 15:06:38 +0000

    Update Handler.js

--- events/Handler.js
+++ events/Handler.js
@@ -3,4 +3,6 @@ module.exports = Pixel => {
   Pixel.on('ready', () => reqEvent('ready')(Pixel));
   Pixel.on('reconnecting', () => reqEvent('reconnecting')(Pixel));
   Pixel.on('disconnect', () => reqEvent('disconnect')(Pixel));
-};
+  Pixel.on('guildCreate', () => reqEvent('guildCreate')(Pixel));
+           
+           };
