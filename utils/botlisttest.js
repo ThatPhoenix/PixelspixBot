7714789d5cdf1d952598bf9ea4208097e19fdc21
Phoenix <theemaraldblock@gmail.com>
2019-03-23 11:55:11 +0000

    Update botlisttest.js

--- utils/botlisttest.js
+++ utils/botlisttest.js
@@ -1,9 +1,9 @@
 const DDBL = require("ddblapi.js");
 const Discord = require('discord.js')
 const Pixel = new Discord.Client();
-  const ddbl = new DDBL(process.env.TOKA); //yeah no stealing my token
+const ddbl = new DDBL(process.env.TOKA); //yeah no stealing my token
     
-  ddbl.postStats("501802312604450820", `${Pixel.guilds.size}`); //Replace "0" to your server_count and "bot_id" to your bot ID
+  ddbl.postStats("501802312604450820", 4); //Replace "0" to your server_count and "bot_id" to your bot ID
 
 
 
